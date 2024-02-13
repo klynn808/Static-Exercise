7321141cf09be515c248ee1b1edac89a2d079f47
Kate Lynn Thomson <katelynn.jaremko@gmail.com>
2024-02-13 06:01:23 -0600

    Exercise 1 commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConvertFahrenheitToCelsius();
+            ConvertCelsiusToFahrenheit();
+        }
+
+        public static void ConvertFahrenheitToCelsius()
+        {
+            Console.WriteLine("Temperature to be converted to celsius: ");
+            var userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out double fahrenheit))
+            {
+                var cels = TempConverter.FahrenheitToCelsius(fahrenheit);
+                Console.WriteLine($"Your Fahrenheit temperature, {fahrenheit} to celsius conversion is {cels:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid temperature in Farhenheit.");
+                ConvertFahrenheitToCelsius(); 
+            }
+        }
+
+        public static void ConvertCelsiusToFahrenheit()
+        {
+            Console.WriteLine("Temperature to be converted to fahrenheit: ");
+            var userInput2 = Console.ReadLine();
+
+            if (double.TryParse(userInput2, out double celsius))
+            {
+                var fahr = TempConverter.CelsiusToFahrenheit(celsius);
+                Console.WriteLine($"Your celsius temperature, {celsius} converted to fahrenheit: {fahr:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid temperature in Celsius.");
+                ConvertCelsiusToFahrenheit(); 
+            }
         }
     }
 }
